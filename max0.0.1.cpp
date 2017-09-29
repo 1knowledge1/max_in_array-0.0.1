0479016fe0f5f6bbf7b5770f2b6d772e9c5c8906
Yaroslav <32066631+1knowledge1@users.noreply.github.com>
2017-09-29 11:27:39 +0300

    Create max0.0.1.cpp

---
+++ max0.0.1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+
+int main()
+{
+    for( std::string string; std::getline( std::cin, string ); ) {
+        int numbers[ 10 ];
+        std::istringstream stream( string );
+        bool failure = false;
+        for( int i = 0; i < 10; ++i ) {
+            if( !( stream >> numbers[ i ] ) ) {
+                failure = true;
+                break;
+            }
+        }
+        int max=numbers[0];
+        for( int j = 0; j < 10; j++ ){
+        	if (numbers[j]>max){
+        		max=numbers[j];
+        	}
+        }
+       
+        
+        if( !failure ) {
+            std::cout<< max;
+        }
+        else {
+            std::cout << "An error has occured while reading numbers from line" << std::endl;
+        }
+       
+    }
+    return 0;
+}
